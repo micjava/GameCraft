43e5a9422a02b2b7ae22fe162f14876ed89e2905
Michael <Michael@Michael_Komp>
2014-09-22 23:33:16 +0200

    Createing Mouse respond

--- src/main/java/gamecraftmaven/pl/vgh/javanerd/Listening.java
+++ src/main/java/gamecraftmaven/pl/vgh/javanerd/Listening.java
@@ -2,8 +2,11 @@ package gamecraftmaven.pl.vgh.javanerd;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class Listening implements KeyListener {
+public class Listening implements KeyListener, MouseListener, MouseMotionListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
@@ -76,5 +79,33 @@ public class Listening implements KeyListener {
 	public void keyTyped(KeyEvent e) {
 
 	}
+
+    public void mouseClicked(MouseEvent e) {
+          
+    }
+
+    public void mousePressed(MouseEvent e) {
+          
+    }
+
+    public void mouseReleased(MouseEvent e) {
+          
+    }
+
+    public void mouseEntered(MouseEvent e) {
+          
+    }
+
+    public void mouseExited(MouseEvent e) {
+          
+    }
+
+    public void mouseDragged(MouseEvent e) {
+          
+    }
+
+    public void mouseMoved(MouseEvent e) {
+          
+    }
 
 }
